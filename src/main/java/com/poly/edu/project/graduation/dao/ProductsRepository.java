edddbcef2ab7ae74944acd5336c601e56f664b55
hungphiitfpt <110955219+hungphiitfpt@users.noreply.github.com>
2022-09-03 09:11:33 +0700

    commit

--- src/main/java/com/poly/edu/project/graduation/dao/ProductsRepository.java
+++ src/main/java/com/poly/edu/project/graduation/dao/ProductsRepository.java
@@ -10,5 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface ProductsRepository extends JpaRepository<ProductsEntity, Integer> {
 //	@Query(value ="{CALL getAllProduct()}" ,nativeQuery = true)
 //	List<ProductDataModel> getAllProducts() throws Exception;
-
+	@Query(value ="select count(idproduct) from products where category = 1 union  select count(idproduct) from products where category = 2 union select count(idproduct) from products where category = 3 " ,nativeQuery = true)
+    List<ProductsEntity> getReport();
 }
